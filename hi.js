1e2814897fc65a651fbb785fd09676ac27d197b6
laviethung <hungla809@gmail.com>
2024-10-09 21:36:06 +0700

    month

---
+++ hi.js
@@ -0,0 +1,11 @@
+let = month = 1;
+    switch (month) {
+        case 1: case 3: case 5:
+            console.log("thang co 31 ngay");
+            break;
+        case 4: case 6: case 8:
+            console.log("thang co 30 ngay");
+        case 2:
+            console.log("thang co 28 hoac 29 ngay");
+            break;
+    }
